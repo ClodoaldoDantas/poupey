586e1172967f5b36b9de0113491ed3139f3226db
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-08-26 22:34:44 -0300

    fix: use Suspense in link component

--- src/components/preserve-query-link.tsx
+++ src/components/preserve-query-link.tsx
@@ -2,12 +2,13 @@
 
 import Link, { type LinkProps } from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 type PreserveQueryLinkProps = LinkProps & {
 	children: React.ReactNode
 }
 
-export function PreserveQueryLink({
+function PreserveQueryLinkContent({
 	children,
 	href,
 	...props
@@ -19,5 +20,13 @@ export function PreserveQueryLink({
 		<Link href={fullHref} {...props}>
 			{children}
 		</Link>
+	)
+}
+
+export function PreserveQueryLink(props: PreserveQueryLinkProps) {
+	return (
+		<Suspense>
+			<PreserveQueryLinkContent {...props} />
+		</Suspense>
 	)
 }
