6a8627a9574a40e62079f111620fcf40978cd8e1
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-08-31 22:00:50 -0300

    feat: add default formatter for JSON files and log transactions in dashboard

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -25,6 +25,8 @@ export default async function DashboardTransactionsPage({
 	const { month, year } = await loadSearchParams(searchParams)
 	const transactions = await getTransactions({ month, year })
 
+	console.log(transactions)
+
 	return (
 		<Card>
 			<CardHeader className="flex flex-wrap gap-4 items-center justify-between">
