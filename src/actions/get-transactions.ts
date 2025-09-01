41440188b9744669f8150e319268b2bc170534a9
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-08-31 22:34:38 -0300

    refactor: restore date filtering logic and update transaction query to use select with left join for categories

--- src/actions/get-transactions.ts
+++ src/actions/get-transactions.ts
@@ -3,7 +3,7 @@
 import dayjs from 'dayjs'
 import { and, desc, eq, gte, lt } from 'drizzle-orm'
 import { db } from '@/db'
-import { transactionsTable } from '@/db/schema'
+import { categoriesTable, transactionsTable } from '@/db/schema'
 import { getSession } from '@/lib/auth'
 import type { Transaction } from '@/types/transaction'
 
@@ -19,37 +19,39 @@ export async function getTransactions({ month, year }: GetTransactionsParams) {
 		throw new Error('Usuário não autenticado.')
 	}
 
-	// const startDate = dayjs()
-	// 	.year(year)
-	// 	.month(month)
-	// 	.startOf('month')
-	// 	.toISOString()
+	const startDate = dayjs()
+		.year(year)
+		.month(month)
+		.startOf('month')
+		.toISOString()
 
-	// const endDate = dayjs().year(year).month(month).endOf('month').toISOString()
+	const endDate = dayjs().year(year).month(month).endOf('month').toISOString()
 
-	const transactions = await db.query.transactionsTable.findMany({
-		columns: {
-			id: true,
-			type: true,
-			amountInCents: true,
-			description: true,
-			paymentDate: true,
-		},
-		where: and(
-			eq(transactionsTable.userId, session.userId),
-			// gte(transactionsTable.paymentDate, startDate),
-			// lt(transactionsTable.paymentDate, endDate),
-		),
-		with: {
+	const transactions = await db
+		.select({
+			id: transactionsTable.id,
+			description: transactionsTable.description,
+			amountInCents: transactionsTable.amountInCents,
+			type: transactionsTable.type,
+			paymentDate: transactionsTable.paymentDate,
 			category: {
-				columns: {
-					id: true,
-					name: true,
-				},
+				id: categoriesTable.id,
+				name: categoriesTable.name,
 			},
-		},
-		orderBy: [desc(transactionsTable.paymentDate)],
-	})
+		})
+		.from(transactionsTable)
+		.leftJoin(
+			categoriesTable,
+			eq(transactionsTable.categoryId, categoriesTable.id),
+		)
+		.where(
+			and(
+				eq(transactionsTable.userId, session.userId),
+				gte(transactionsTable.paymentDate, startDate),
+				lt(transactionsTable.paymentDate, endDate),
+			),
+		)
+		.orderBy(desc(transactionsTable.paymentDate))
 
 	return transactions as Transaction[]
 }
